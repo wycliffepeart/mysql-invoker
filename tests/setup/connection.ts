338a31306636be15dc3bbcb62159cd7e08a58574
Wycliffe Peart <wycliffepeart@gmail.com>
2019-06-24 18:45:14 -0500

    isolate connection config

---
+++ tests/setup/connection.ts
@@ -0,0 +1,9 @@
+
+
+export const connectionCongif = () => ({
+    host: 'localhost',
+    user: 'application', 
+    password: 'secret', 
+    database: 'organization', 
+    multipleStatements: true 
+})
