3f3d1e2c9dfdcdc189733682650f590512abf168
Wycliffe Peart <wycliffepeart@gmail.com>
2019-06-27 20:11:48 -0500

    update proxy target

--- src/function.repository.ts
+++ src/function.repository.ts
@@ -1,6 +1,6 @@
 import * as Mysql from 'mysql';
+import {ActionParams} from './contracts';
 import {MysqlInvoker} from './mysql.invoker';
-import { ActionParams } from './contracts';
 
 /**
  * Run the given action against the mysql database store procedure
@@ -8,24 +8,24 @@ import { ActionParams } from './contracts';
  * @param {string} connectionConfig
  * @return {Promise<T>}
  */
-export function CreateFunctionRepository<T>(connectionConfig: Mysql.PoolConfig | Mysql.ConnectionConfig = {}): T{
-
-    return new Proxy<any>({connectionConfig}, {
+export function CreateFunctionRepository<T>(connectionConfig: Mysql.PoolConfig | Mysql.ConnectionConfig = {}): T {
   
-      get: function (connectionConfig: Mysql.PoolConfig | Mysql.ConnectionConfig, prop: string): (...models: ActionParams) => Promise<T> {
+  return new Proxy<any>(connectionConfig, {
+    
+    get: function (connectionConfig: Mysql.PoolConfig | Mysql.ConnectionConfig, prop: string): (...models: ActionParams) => Promise<T> {
+      
+      return function (...models: ActionParams): Promise<T> {
+        
+        console.log({connectionConfig, prop, models});
+        
+        const mysql = new MysqlInvoker(connectionConfig);
+        
+        return mysql.invokeFunction(prop, ...models);
+        
+      };
+      
+    }
+    
+  })
   
-          return function (...models: ActionParams): Promise<T> {
-  
-            console.log({ connectionConfig, prop, models });
-
-            const mysql = new MysqlInvoker(connectionConfig);
-            
-            return mysql.invokeFuncton(prop, ...models);
-  
-          };
-  
-      }
-  
-    })
-  
-  }
+}
